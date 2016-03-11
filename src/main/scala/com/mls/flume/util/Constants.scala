0b0b9e4ac3a319a10e2b729eda3eb090376b9629
zhikuanzhang <zhikuanzhang@meilishuo.com>
2016-03-11 17:04:53 +0800

    修改代码

--- src/main/scala/com/mls/flume/util/Constants.scala
+++ src/main/scala/com/mls/flume/util/Constants.scala
@@ -10,7 +10,7 @@ object Constants {
   val TOPIC_SPLIT = "_"
 
   //时间模版
-  private val dateFormat = new SimpleDateFormat("yyyyMMddHH:mm")
+  private val dateFormat = new SimpleDateFormat("yyyyMMddHHmm")
 
   def date2String(time: Long): String = {
     dateFormat.format(new Date(time))
